7c09d5661c813d6ea00a76d47249c4733150ba65
WilliamDeckmann <u773560@rts-365.dk>
2022-02-01 11:24:12 +0100

    Add basic page structure

--- src/templates/App.js
+++ src/templates/App.js
@@ -5,19 +5,31 @@ import { Routes, Route } from "react-router-dom";
 import ControlPanel from "../pages/ControlPanel";
 import BedRoom from "../pages/BedRoom";
 
+// Templates
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<ControlPanel />} />
-        <Route path="/bed-room" element={<BedRoom />} />
-        <Route path="/living-room" element={<BedRoom />} />
-        <Route path="/kitchen" element={<BedRoom />} />
-        <Route path="/bathroom" element={<BedRoom />} />
-        <Route path="/outdoor" element={<BedRoom />} />
-        <Route path="/balcony" element={<BedRoom />} />
-      </Routes>
+      <Header>
+        Header
+      </Header>
+      <Main>
+        <Routes>
+          <Route path="/" element={<ControlPanel />} />
+          <Route path="/bed-room" element={<BedRoom />} />
+          <Route path="/living-room" element={<BedRoom />} />
+          <Route path="/kitchen" element={<BedRoom />} />
+          <Route path="/bathroom" element={<BedRoom />} />
+          <Route path="/outdoor" element={<BedRoom />} />
+          <Route path="/balcony" element={<BedRoom />} />
+        </Routes>
+      </Main>
+      <Footer>
+        Footer
+      </Footer>
     </div>
   );
 }
---
+++ src/templates/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+    return (
+        <div className="Header">
+            {props.children}
+        </div>
+    );
+}
+ 
+export default Header;

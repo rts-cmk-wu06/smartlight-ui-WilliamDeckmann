11e25b3a8fc243cb3168c2cb2b73617238a88ca4
WilliamDeckmann <u773560@rts-365.dk>
2022-02-02 12:29:19 +0100

    Update navigation & add "navigaiton-link"

--- src/templates/Navigation.js
+++ src/templates/Navigation.js
@@ -1,20 +1,23 @@
-// Imports
-import { Link } from "react-router-dom";
-
 // Json
 import Data from "../json/navigation.json"
+
+// Assets
+import Lightbulb from "../assets/lightbulb.png"
+import House from "../assets/house.png"
+import Gear from "../assets/gear.png"
+
+// Components
+import NavigationLink from "../components/NavigationLink";
 
 const Navigation = () => {
     return (
         <nav className="Navigation">
             <ul className="Navigation__list flex gap-16">
-                {Data.map(item => (
-                    <li className="Navigation__item" key={item.id}>
-                        <Link to={item.url}>
-                            Link
-                        </Link>
-                    </li>
-                ))}
+                {/* {Data.map(item => ( */}
+                    <NavigationLink url="" alt="light bulb" src={Lightbulb} />
+                    <NavigationLink url="" src="house" src={House} />
+                    <NavigationLink url="" src="gear" src={Gear} />
+                {/* ))} */}
             </ul>
         </nav>
     );

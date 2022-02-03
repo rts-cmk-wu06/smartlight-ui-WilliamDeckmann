6e6191a194a603e73daf799d34a6349614c59b16
WilliamDeckmann <u773560@rts-365.dk>
2022-02-03 13:05:05 +0100

    Add basic text components

---
+++ src/components/TextMedium.js
@@ -0,0 +1,9 @@
+const TextMedium = (props) => {
+    return (
+        <h2 className="Heading-medium text-xl font-medium">
+            {props.text}
+        </h2>
+    );
+}
+ 
+export default TextMedium;

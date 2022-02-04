fab07ff413ff9b2d693df19e6562ab643b6b4385
WilliamDeckmann <u773560@rts-365.dk>
2022-02-04 13:25:09 +0100

    Update color-selector

--- src/components/ColorAdder.js
+++ src/components/ColorAdder.js
@@ -1,4 +1,4 @@
-const ColorAdder = (props) => {
+const ColorAdder = () => {
     return (
         <button className="Color-button h-7 w-7 flex justify-center items-center rounded-full bg-colorIcon-white" >
             <p className="Color-button text-base font-bold">
